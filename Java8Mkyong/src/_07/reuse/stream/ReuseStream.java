29a049d158c311063af8d55bfb0db882119021bb
leventerguder <erguder.levent@gmail.com>
2017-05-06 13:58:40 +0300

    reuse stream

--- Java8Mkyong/src/_07/reuse/stream/ReuseStream.java
+++ Java8Mkyong/src/_07/reuse/stream/ReuseStream.java
@@ -1,8 +1,29 @@
 package _07.reuse.stream;
 
+import java.util.Arrays;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 public class ReuseStream {
 
 	public static void main(String[] args) {
-		
+		String[] array = { "a", "b", "c", "d", "e" };
+
+		Stream<String> stream = Arrays.stream(array);
+
+		stream.forEach(System.out::println);
+
+		// java.lang.IllegalStateException: stream has already been operated
+		// upon
+		// or closed
+		// long count = stream.count();
+
+		// you can use Supplier
+
+		Supplier<Stream<String>> streamSupplier = () -> Stream.of(array);
+		//
+		streamSupplier.get().forEach(System.out::println);
+		long count = streamSupplier.get().count();
+		System.out.println(count);
 	}
 }
