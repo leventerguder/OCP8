4451367d0e70bbe09bc5076ec677bd5f7591f509
leventerguder <erguder.levent@gmail.com>
2017-11-29 18:41:58 +0300

    chapter3

--- OCP8StudyGuide/src/ch03/_03/collections/ArrayDequeTest.java
+++ OCP8StudyGuide/src/ch03/_03/collections/ArrayDequeTest.java
@@ -6,11 +6,11 @@ import java.util.Queue;
 public class ArrayDequeTest {
 
 	public static void main(String[] args) {
-		
+
 		Queue<Integer> queue = new ArrayDeque<>();
-		System.out.println(queue.offer(10)); //10
+		System.out.println(queue.offer(10)); // 10
 		System.out.println(queue.add(20)); // 10 20
-		System.out.println(queue.offer(30)); //10 20 30
+		System.out.println(queue.offer(30)); // 10 20 30
 		System.out.println(queue);
 		System.out.println(queue.peek());
 		System.out.println(queue.poll());
@@ -18,12 +18,17 @@ public class ArrayDequeTest {
 		System.out.println(queue.peek());
 		System.out.println(queue.peek());
 
+		System.out.println();
 		//
 		ArrayDeque<Integer> stack = new ArrayDeque<>();
 		stack.push(100);// 100
-		stack.push(20); // 20 100 
+		stack.push(20); // 20 100
 		stack.offer(40); // 20 100 40
-		//stack.add(null);// NullPointerException
+		System.out.println(stack.remove());
+		System.out.println(stack.poll());
+		System.out.println(stack.poll());
+
+		// stack.add(null);// NullPointerException
 		System.out.println(stack);
 	}
 }
